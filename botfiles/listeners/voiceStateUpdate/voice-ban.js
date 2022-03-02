fb1edda83fe77f6f160410d1e0e05b5a048d0f04
adiiityaaa <adityak3653@gmail.com>
2022-03-02 18:20:34 +0530

    Added the event for Voice Ban

--- botfiles/listeners/voiceStateUpdate/voice-ban.js
+++ botfiles/listeners/voiceStateUpdate/voice-ban.js
@@ -1,2 +1,6 @@
 module.exports.run = async(client, oS, nS) => {
-}    
+if(!oS.channel && nS.channel) {
+if(client.db.has(`voiceban_${nS.member.id}_${nS.guild.id}`)) { nS.member.voice.disconnect() }
+const embed = client.modules.embed(client, client.colors.red, `${client.emotes.cross} | **<@${nS.member.id}> tried to join voice channel.**`)
+if(client.db.has(`voicelogs_${nS.guild.id}`)) { client.channels.cache.get(client.db.get(`voicelogs_${nS.guild.id}`)).send({ embeds: [embed] }) }
+}}    
