ab56c801cabce839451863317952fd6888da2481
adiiityaaa <adityak3653@gmail.com>
2022-03-02 19:14:00 +0530

    Edited openai is not defined

--- botfiles/listeners/messageCreate/chatbot.js
+++ botfiles/listeners/messageCreate/chatbot.js
@@ -1,8 +1,9 @@
 module.exports.run = async(client, message) => {
 if(client.db.has(`chatbot_${message.guild.id}`)) {
-const completion = await openai.createCompletion("text-davinci-001", {
+if(message.channel.id === client.db.get(`chatbot_${message.guild.id}`)) {
+const completion = await client.openai.createCompletion("text-davinci-001", {
         prompt: message.content,
 });
 const responseContent = response.data.choices[0].text.split(":").at(-1).trim();
 message.reply({ content: `${responseContent}`, allowedMentions: { repliedUser: false } })
-}}
+}}}
