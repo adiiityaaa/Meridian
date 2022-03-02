1c1b310b48c095b5ebb2c4938766ca351f5db7e7
adiiityaaa <adityak3653@gmail.com>
2022-03-02 19:27:26 +0530

    Return if message.author is a bot.

--- botfiles/listeners/messageCreate/chatbot.js
+++ botfiles/listeners/messageCreate/chatbot.js
@@ -1,4 +1,5 @@
 module.exports.run = async(client, message) => {
+if(message.author.bot) { return; }    
 if(client.db.has(`chatbot_${message.guild.id}`)) {
 if(message.channel.id === client.db.get(`chatbot_${message.guild.id}`)) {
 const response = await client.openai.createCompletion("text-davinci-001", {
