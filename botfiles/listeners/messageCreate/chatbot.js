ecbe5443a5a87b282a4a2cb8222239943f991d83
adiiityaaa <adityak3653@gmail.com>
2022-03-03 15:43:11 +0530

    Improved the AI Responses.

--- botfiles/listeners/messageCreate/chatbot.js
+++ botfiles/listeners/messageCreate/chatbot.js
@@ -4,6 +4,11 @@ if(client.db.has(`chatbot_${message.guild.id}`)) {
 if(message.channel.id === client.db.get(`chatbot_${message.guild.id}`)) {
 const response = await client.openai.createCompletion("text-davinci-001", {
         prompt: message.content,
+        temperature: 0.7,
+        max_tokens: 64,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
 });
 const responseContent = response.data.choices[0].text;
 message.reply({ content: `${responseContent}`, allowedMentions: { repliedUser: false } })
