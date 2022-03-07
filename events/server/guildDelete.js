2e9ba7279098a5ccf67d62909793cc6d6b9c3878
adiiityaaa <adityak3653@gmail.com>
2022-03-07 10:41:05 +0530

    Resolved a bug causing Major error.

--- events/server/guildDelete.js
+++ events/server/guildDelete.js
@@ -1,5 +1,5 @@
 module.exports.run = async(client, guild) => {
-if(!client.isReady()) { return; }    
+if(!client.isReady() || !guild.available) { return; }    
 const embed = client.modules.embed(client, client.colors.red, `${client.emotes.exit} | **Left a Guild!**\n${client.emotes.rarrow} **Name:** ${guild.name}\n${client.emotes.rarrow} **Members:** ${guild.memberCount}\n${client.emotes.rarrow} **New Server Count:** ${client.guilds.cache.size}`)
 client.channels.cache.get(client.settings.serverlog).send({ embeds: [embed] })
 }
