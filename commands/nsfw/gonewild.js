2a810e207be33161684a67654c481c384aa5cc97
adiiityaaa <adityak3653@gmail.com>
2022-04-29 09:11:25 +0530

    NSFW Base Commitment

---
+++ commands/nsfw/gonewild.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: "",
+    category: "NSFW",
+    description: "",
+    type: 1,
+    developerOnly: false,
+    voiceChannel: false,
+    mutualChannel: false,
+    djOnly: false,    
+    clientPerms: ["EMBED_LINKS", "USE_EXTERNAL_EMOJIS"],
+    authorPerms: [""],
+    run: async(client, interaction) => {
+        
+    }
+   }
+   
